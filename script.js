cd948bcf70c96ef790efb0c1db6e6246caa42296
LUIS_HERRERA <luis.herrera.dev@gmail.com>
2022-06-07 22:53:05 -0500

    Se modifica informacion y se agrega boton para calcular porcentaje

--- script.js
+++ script.js
@@ -12,6 +12,21 @@
 
  function calcular(value){
    console.log(value)
+   
+
+   if (value.includes('%')) {
+     console.log('si incluye')
+     let arrayPercent = value.split('%')
+     let percent = arrayPercent[0]
+     let percentTotal = arrayPercent[1]
+
+     console.log(arrayPercent)
+
+     let result = (percentTotal / 100) * percent 
+     console.log(result)
+     form.displayResult.value = result
+     return;
+   }
   if (value) {
     form.displayResult.value=eval(value);
   }
